6aadca70176334049eed706ab57d6c31c12b2559
Mahmoud Nasr <239.nasr@gmail.com>
2025-05-04 14:52:15 +0300

    Refactor Payment model and update database schema
    - Changed `Status` property type from `string` to `int` in `AppDBModelSnapshot.cs` and `Payment.cs`, introducing a new `PaymentStatus` enum.
    - Added new properties: `ExpirationDate`, `UpdatedAt`, and `PaymentDate` to the `Payment` entity.
    - Updated `Subscription.cs` to set `IsActive` default value to `false`.
    - Created migration `20250504115055_Payment Status.cs` to reflect changes in the `Payments` table.
    - Updated designer file `20250504115055_Payment Status.Designer.cs` to ensure schema consistency.

--- Models/Payment.cs
+++ Models/Payment.cs
@@ -1,4 +1,6 @@
-﻿namespace Shortha.Models
+﻿using Shortha.DTO;
+
+namespace Shortha.Models
 {
     public class Payment
     {
@@ -7,13 +9,15 @@
         public virtual AppUser User { get; set; } = null!;
         public DateTime? PaymentDate { get; set; } = null;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+        public DateTime ExpirationDate { get; set; } = DateTime.UtcNow.AddHours(1);
         public decimal Amount { get; set; }
         public string? Currency { get; set; } = null;
         public string? PaymentMethod { get; set; } = null;
         public string? TransactionId { get; set; } = null;
-        public string? Status { get; set; } = null;
+        public PaymentStatus Status { get; set; } = PaymentStatus.Pending;
 
-        public Guid SubscriptionId { get; set; } 
+        public Guid SubscriptionId { get; set; }
         public virtual Subscription Subscription { get; set; } = null!;
     }
 }
--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -9,7 +9,7 @@
 
         public DateTime StartDate { get; set; } = DateTime.UtcNow;
 
-        public bool IsActive { get; set; } = true;
+        public bool IsActive { get; set; } = false;
 
         public virtual Package Package { get; set; } = null!;
         public int PackageId { get; set; }
